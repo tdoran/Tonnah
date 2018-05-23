7f53b4012d00326ad4840e5ba81bb1acc6001c5d
Jennah <contactjennah@gmail.com>
2018-05-23 14:57:34 +0100

    Move files into public and src folders creating App.js file

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "./header";
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+                <Header />
+            </React.Fragment>
+        )
+    }
+}
