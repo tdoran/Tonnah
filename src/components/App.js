4c049ed0ee9fb572921a0b92ce9d658cdcdde6f0
Jennah <contactjennah@gmail.com>
2018-05-23 15:26:34 +0100

    Create and render board component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Header from "./header";
+import React from "react";
+import Board from "./board/board.js";
 
 export default class App extends React.Component {
     render() {
         return(
             <React.Fragment>
-                <Header />
+                <Board />
             </React.Fragment>
         )
     }
