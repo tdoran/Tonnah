cb66a3606337f10da64ac901fb5942231368ad41
tdoran <tomgdoran@gmail.com>
2018-05-24 16:16:33 +0100

    added api call and functionality

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -3,21 +3,14 @@ import Timer from "./timer/timer.js";
 import Singlephoto from "./singlephoto/singlephoto.js";
 import Photogroup from "./photogroup/photogroup.js";
 import Gameover from "./gameover/gameover.js";
-import { pickSingle } from "../../utils/datahelpers.js";
-
-let photoGroupApi = [
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/1200px-Hot_dog_with_mustard.png",
-  "https://content.freddysusa.com/wp-content/uploads/2016/03/veggie-burger.png",
-  "http://kannii.com/wp-content/uploads/2017/07/chips.png",
-  "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2013/01/17/Production/Sunday/SunBiz/Images/268727_254043381379945_546402058_n.tif?t=20170517"
-];
+import { getData, pickSingle, makeImageArray } from "../../utils/datahelpers.js";
 
 export default class Board extends React.Component {
   state = {
     score: 0,
     time: 5,
     singlePhoto: "",
-    photoGroup: photoGroupApi,
+    photoGroup: '',
     renderSinglePhoto: false,
     renderPhotoGroup: false,
     url: "",
@@ -47,6 +40,14 @@ export default class Board extends React.Component {
   };
 
   beginGame = () => {
+    getData()
+      .then(data => {
+        console.log('data')
+        let gifArray = makeImageArray(data);
+        this.setState({ photoGroup: gifArray })
+      })
+
+    // console.log('gifArray: ', gifArray)
     this.setState(() => {
       let pickedPhoto = pickSingle(this.state.photoGroup);
       return {
