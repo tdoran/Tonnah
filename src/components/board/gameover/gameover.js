f7249049f7f7321e7cdc9f8646dc4a3ef1c62253
Jennah <contactjennah@gmail.com>
2018-05-24 12:44:35 +0100

    Implement win/lose functionality

---
+++ src/components/board/gameover/gameover.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Gameover extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> Game Over! </h2>
+        <p> Your score was {this.props.score}</p>
+      </div>
+    );
+  }
+}
