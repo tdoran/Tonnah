cf44c3f78529f0081db13b1b37be574e84fd3894
Jennah <contactjennah@gmail.com>
2018-05-25 12:47:36 +0100

    Change value of key so it does not use array index relates #14

--- src/components/board/photogroup/photogroup.js
+++ src/components/board/photogroup/photogroup.js
@@ -3,13 +3,14 @@ import "./style.css";
 
 export default class Photogroup extends React.Component {
   render() {
+    let i = 0;
     const { allPhotos } = this.props;
     return (
       <div>
         {allPhotos.map(photo => (
           <img
             className="board--photos"
-            key={allPhotos.indexOf(photo)}
+            key={i++}
             src={photo[0]}
             alt={photo[1]}
             onClick={this.props.clickHandler(photo)}
