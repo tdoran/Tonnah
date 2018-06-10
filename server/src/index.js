46c5175f40bfc10b497a086462f7a8a38e554df4
tdoran <tomgdoran@gmail.com>
2018-06-10 22:28:57 +0100

    initial server setup

---
+++ server/src/index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("axios");
+
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 3000;
+const token = process.env.API_KEY;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`app is running on http://${host}:${port}`);
+});
+
+// config middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(_, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:1234");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const checkResponse = response => {
+  if (response.status !== 200) {
+    console.log(`Error with the request! ${response.status}`);
+    return;
+  }
+  return response;
+};
+
+// getGifs route
+app.get("/getGifs", (req, res, next) => {
+  return request
+    .get(`http://api.giphy.com/v1/stickers/trending?limit=200&api_key=${token}`)
+    .then(gifResponse => {
+      console.log("Success", gifResponse.data.data);
+      res.json(JSON.stringify(gifResponse.data));
+    })
+    .catch(err => {
+      console.log("error: ", err.message);
+      return res.json({
+        error: "request error"
+      });
+    });
+});
